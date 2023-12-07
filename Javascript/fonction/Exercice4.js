175ec00d45ff81e8c6c4d85af87cacb435803b1d
Abdourazack Adel <72739188+Abdourazack@users.noreply.github.com>
2023-12-07 17:10:48 +0100

    push

--- Javascript/fonction/Exercice4.js
+++ Javascript/fonction/Exercice4.js
@@ -1,12 +1,20 @@
 //Exercice 4
 
 //Écrire une fonction qui prend un nombre en entrée et renvoie true si le nombre est pair, et false s'il est impair.
-const nombre = 13
-const impaire = function(){
-    if(nombre % 2 == 1 ){
-        return true + ` ${nombre} est impaire`
-    }else{
-        return false + ` ${nombre} est paire`
-    }
-}
-console.log(impaire(nombre))
+
+const{input}= require('@inquirer/prompts');
+
+(async function(){
+    const nombre = await input({message : `saisissez un nombre ? `})
+    const impaire = function(){
+        if(nombre % 2 == 1 ){
+            return true + ` ${nombre} est impaire`
+        }else{
+            return false + ` ${nombre} est paire`
+        }
+       
+    } 
+    console.log(impaire(nombre))
+         
+})()
+
