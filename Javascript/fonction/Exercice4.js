e4811b797b53f1ddc965813d3dc4bb3c748b150c
Abdourazack Adel <72739188+Abdourazack@users.noreply.github.com>
2023-12-07 03:56:23 +0100

    push

--- Javascript/fonction/Exercice4.js
+++ Javascript/fonction/Exercice4.js
@@ -1,16 +1,12 @@
 //Exercice 4
 
 //Écrire une fonction qui prend un nombre en entrée et renvoie true si le nombre est pair, et false s'il est impair.
-const nombre = 15
+const nombre = 13
 const impaire = function(){
-
-    
-
     if(nombre % 2 == 1 ){
-        console.log('true')
+        return true + ` ${nombre} est impaire`
     }else{
-        console.log('false')
+        return false + ` ${nombre} est paire`
     }
-    return nombre
 }
 console.log(impaire(nombre))
